7242350c86a5e0624a53527b530bc6dbbda04833
unknown <aubrey_jones07@hotmail.com>
2020-12-13 21:38:46 -0700

    Add quizzes.map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 import QuizQuestions from './QuizQuestions'
-
-
-
-
-
-
-
-
 
 
 
--- src/QuizQuestions.js
+++ src/QuizQuestions.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import quizinfoData from './data/quizquestions.json'
 import SideNavigation from './SideNavigation'
+import QuizResponses from './QuizResponses'
 import quizzesStyles from './css/quizzesStyles.css'
 //import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 //import {faUser, faBook, faFile, faSquare, faTrophy} from '@fortawesome/free-solid-svg-icons'
 //import {AccountCircle, Dashboard, LibraryBooks, Extension, HourglassFull, Grade} from '@material-ui/icons'
 //import Icon from '@material-ui/core/icon'
-//import {CardMedia} from '@material-ui/core'
+import {Card} from '@material-ui/core'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { render } from '@testing-library/react'
 
 
 const quizzes = quizinfoData.results[0].questions
 
 
 
-function QuizQuestions() {
+class QuizQuestions extends Component {
 
 
+   
 
+/*   state = {
+quizzes: quizinfoData.results[0].questions
+
+
+}
+*/
+
+render() {
 return (
+
+
+
+<div>
 <html>
 <header>
 
@@ -33,36 +48,91 @@ return (
 <div className="col-1">
 <SideNavigation></SideNavigation>
 </div>
-<div className="col-6">  
-<h1>This is a Quiz</h1> 
+<div className="col-sm-7 col-md-7 col-lg-7"> 
 
 
-{quizzes.map(quiz => {
-return (<div className="questionInfo">
- <h1>{quiz.number}</h1>  
+<div className="quizBreadcrumbs">
+<ul>
+<li><a href="#">Quizzes</a></li>
+<li>/</li>
+<li><a href="#"> Unit 1</a></li>
+<li>/</li>
+<li><a href="#"> Quiz 1</a></li>
+</ul>
+
+
+</div>
+
+
+
+
+{
+   
+quizzes.map((quiz) => {
+return (   
+
+
+
+
+<Card className="questionInfo">
+
+<h1>{quiz.number}</h1>
 <h2>{quiz.question}</h2>
 <p>{quiz.questionDetail}</p>
-<button>{quiz.answers.a}</button>
-<button>{quiz.answers.b}</button>
-<button>{quiz.answers.c}</button>
+<div className="allButtons">
+<div id="choiceOne" onClick={()=> question1()}>{quiz.option1}</div>
+<div id="choiceTwo" onClick={()=> question2()}>{quiz.option2}</div>
+<div id="choiceThree" onClick={()=> question3()}>{quiz.option3}</div>
+</div>
+</Card>
 
 
-</div>)
 
+)
+
+
+const question1 = () => {
+   console.log("Congrats, You got it right!")
+      
+   }
+
+   const question2 = () =>{
+      console.log("Sorry, that is wrong. Try again?")
+         
+      }
+      
+      
+      const question3 = () => {
+   console.log("Sorry, that is wrong. Try again?")
+      
+   }
+   
 
 })
+
+
+
+
+
 }
+
+
+
+
+
+
+<div id="userFeedback"></div>
 
 </div> 
 
-<div className="col-3">
+<div className="col-sm-4 col-md-3 col-lg-3">
 <ul className="questionNavigation">
-<li><a>QUESTION 1</a></li>
-<li><a>QUESTION 2</a></li>
-<li><a>QUESTION 3</a></li>
-<li><a>QUESTION 4</a></li>
-<li><a>QUESTION 5</a></li>
-<li><a>QUESTION 6</a></li>
+<li><a href="question1">QUESTION 1</a></li>
+<li><a href="question2">QUESTION 2</a></li>
+<li><a href="question3">QUESTION 3</a></li>
+<li><a href="question4">QUESTION 4</a></li>
+<li><a href="question5">QUESTION 5</a></li>
+<li><a href="question6">QUESTION 6</a></li>
 <button className="submitButton">SUBMIT</button>
 </ul>
 
@@ -81,12 +151,18 @@ return (<div className="questionInfo">
 
 </html>
 
+</div>
+
+
+
 
 
 )
 
-
+}
 
 }
+
+
 
 export default QuizQuestions
